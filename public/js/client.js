8c2cb6213607a650afdffe1a57fe4e6502e3d1c1
reneforstner <61466472+reneforstner@users.noreply.github.com>
2020-06-05 10:17:11 +0200

    Update client.js

--- public/js/client.js
+++ public/js/client.js
@@ -93,7 +93,7 @@ function order() {
     var success = false;
     $.ajax({
         //url: "http://orders",
-	url: "orders",
+	url: "http://orders.dev.svc.cluster.local",
         type: "POST",
         async: false,
         success: function (data, textStatus, jqXHR) {
